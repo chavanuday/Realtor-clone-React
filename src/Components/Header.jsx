2b1d9e6301448282aa3bc3328981816173f5612c
Uday Chavan Git <udaymchavan228@gmail.com>
2023-06-03 04:30:40 +0530

    create sign in, sign up and forgot password pages and OAuth component

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,21 +1,21 @@
 import React from "react";
-import { useLocation, useNavigate } from "react-router";
+
+import { useLocation, useNavigate } from "react-router-dom";
 
 export default function Header() {
   const location = useLocation();
   const navigate = useNavigate();
   console.log(location.pathname);
+
   function pathMathRoute(route) {
     if (route === location.pathname) {
       return true;
     }
   }
+
   return (
     <div className="bg-white border-b shadow-sm sticky top-0 z-50">
-      <header
-        className="flex justify-between items-cen
-      px-3 max-w-6xl mx-auto "
-      >
+      <header className="flex justify-between items-center px-3 max-w-6xl mx-auto ">
         <div>
           <img
             src="https://static.rdc.moveaws.com/images/logos/rdc-logo-default.svg"
@@ -29,7 +29,7 @@ export default function Header() {
           <ul className="flex space-x-10">
             <li
               className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${
-                pathMathRoute("/") && "text-black  border-b-red-500"
+                pathMathRoute("/") && "text-black border-b-red-500"
               } `}
               onClick={() => navigate("/")}
             >
